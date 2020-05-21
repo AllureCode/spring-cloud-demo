78ad83fe3288cfcfa445ca3b0a99e2d8069ca0ae
15398069231 <51853925+15398069231@users.noreply.github.com>
2020-05-21 19:44:53 +0800

    insert new content[algorithm-randomRule]

--- springcloud/microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/gnawococ/springcloud/cfgbeans/ConfigBean.java
+++ springcloud/microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/gnawococ/springcloud/cfgbeans/ConfigBean.java
@@ -1,9 +1,12 @@
 package com.gnawococ.springcloud.cfgbeans;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
 
 /**
  * @program: springcloudProject
@@ -17,6 +20,20 @@ public class ConfigBean {
     @Bean
     @LoadBalanced   //添加负载均衡
     public RestTemplate getRestTemple(){
+
         return new RestTemplate();
     }
+
+    /**
+     *@Author:  wang_sir
+     * @Description: 采用随机算法替代默认的轮询算法
+     * @Date: 19:34 2020/5/21
+     * @Param: []
+     * @return: com.netflix.loadbalancer.IRule
+     **/
+    @Bean
+    public IRule myRule(){
+        return new RandomRule();
+    }
+
 }
